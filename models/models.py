e8be55563e111e7cd992c0b051d922124156d20a
odic30 <sanchezernis@gmail.com>
2021-02-05 13:47:54 -0500

    colocamos los menus de manera simple

--- models/models.py
+++ models/models.py
@@ -2,14 +2,16 @@
 
 from odoo import models, fields, api
 
-# class compara2(models.Model):
-#     _name = 'compara2.compara2'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class walmartProducto(models.Model):
+     _name = 'walmart.producto'
+
+class amazonProducto(models.Model):
+     _name = 'amazon.producto'
+     
+    
+class productComparador(models.Model):
+     _name = 'products.comparador'
+     
+     productoWmt = fields.Many2one('walmart.product',string='Producto Walmart')
+     productoAmz = fields.Many2one('amazon.product',string='Producto Amazon')
